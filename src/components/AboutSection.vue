7eaa1c635b2d27ba50896e0c1e5bf6c68d997f0a
Pedro Bustilho <pedrobustilho@gmail.com>
2024-10-06 21:18:54 -0300

    Primeiro commit inclui projeto no repositorio

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <section id="about">
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="7">
+              <h1 class="font-weight-light display-2">Como funciona?</h1>
+              <v-row class="mt-5">
+                <v-col cols="12" class="d-flex align-center">
+                  <p class="text-justify">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  </p>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="12" md="5" class="d-none d-md-flex">
+              <v-img
+                src="@/assets/img/tampinha.png"
+                class="d-block ml-auto mr-auto"
+                max-width="400px"
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
+
+<style scoped>
+#about {
+  background-color: #f4f7f5;
+}
+</style>
